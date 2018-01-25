fddfd8dc4bd19a6a8b31c4b00289c6b71f18c4fc
Hytham Safieh <hytham.a@gmail.com>
2018-01-25 14:17:52 -0600

    Add files via upload

---
+++ create-org.py
@@ -0,0 +1,37 @@
+import sys, json, requests, csv, datetime
+
+
+def readme():
+    print("******************************************************************************************************************")
+    print("")
+    print("Create an orgnization or multiple organizations by reading input from a JSON file")
+    print("")
+    print("Prior to script execution please be sure to correctly fill in the orgID.json file with the correct key value pairng")
+    print("You will find the necessary file to be modified in the current working directory")
+    print("")
+    print("******************************************************************************************************************")
+
+if __name__ == '__main__':
+    try:
+        import input
+        key = input.key
+    except ImportError:
+        key = str(raw_input('Enter your Meraki API key: '))
+
+    head = {
+            'Content-type': 'application/json',
+            'X-Cisco-Meraki-API-Key': key
+           }
+
+    uri = "https://api.meraki.com/api/v0/organizations/"
+    data = json.load(open('orgID.json', 'r'))
+        
+    
+    #######################################################
+    # Create New Organizations
+    #######################################################
+    try:
+        for i in data:
+            org = json.loads(requests.post(uri,params=i,headers=head).content)
+    except:
+        sys.exit(readme())
