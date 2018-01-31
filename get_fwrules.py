c885462d2f6131c699e2750a29c1bffb699734de
Hytham Safieh <hytham.a@gmail.com>
2018-01-30 18:29:31 -0600

    Retrieve FW Rules from a single organization

---
+++ get_fwrules.py
@@ -0,0 +1,73 @@
+import csv, json, requests, datetime
+
+try:
+    import input
+    key = input.key
+    org_name = input.org_name
+    net_name = input.net_name
+except ImportError:
+    print('Looks like there was an error inputting your variables. Please enter the correct informatino now: ')
+    key = str(raw_input('Enter your Meraki API key: '))
+    org_name = str(raw_input('Enter the organization name you would like to search: '))
+    net_name = str(raw_input('Enter the network name you are searching for: '))
+except AttributeError:
+    print('Looks like there was an error inputting your variables. Please enter the correct information now: ')
+    key = str(raw_input('Enter your Meraki API key: '))
+    org_name = str(raw_input('Enter the organization name you would like to search: '))
+    net_name = str(raw_input('Enter your network name you are searching for: '))
+
+head = {
+        'content-type': 'application/json',
+        'x-cisco-meraki-api-key': key
+        }
+
+today = datetime.date.today()
+baseuri = 'https://api.meraki.com/api/v0/'
+
+
+def main():
+
+    # Grab organization ID
+    getorgid = json.loads(requests.get(baseuri + 'organizations/', headers=head).content)
+
+    for orgname in getorgid:
+        if orgname['name'] == org_name:
+            orgid = orgname['id']
+
+    # Grab the Network ID
+    getnetid = json.loads(requests.get(baseuri + 'organizations/' + `orgid` + '/networks/',headers=head).content)
+
+    for netname in getnetid:
+        if netname['name'] == net_name:
+            netid = netname['id']
+    
+    
+    #print("")
+    #print ('The OrgID for %s is %s and the requested associated network is %s') % (org_name, orgid, netid)
+
+    # Define CSV file write paramters
+    with open (net_name + '_' 'FWRules_' + str(today) + '.csv', 'w') as csvfile:
+        fieldnames = ['Comment', 'Policy', 'Protocol(s)', 'Source Port(s)', 'Source Address(es)', 'Destination Port(s)', 'Destination Address(es)', 'Syslog']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+    # Grab firewall rules
+        getfwrules = json.loads(requests.get(baseuri + 'networks/' + netid + '/l3FirewallRules/',headers=head).content)
+
+        for i in getfwrules:
+
+       
+            comment = i['comment']
+            policy = i['policy']
+            protocol = i['protocol']
+            destport = i['destPort']
+            destcidr = i['destCidr']
+            srcport = i['srcPort']
+            srccidr = i['srcCidr']
+            syslog = i['syslogEnabled']
+
+            writer.writerow({'Comment': comment, 'Policy': policy, 'Protocol(s)': protocol, 'Source Port(s)': srcport, 'Source Address(es)': srccidr, 'Destination Port(s)': destport, 'Destination Address(es)': destcidr, 'Syslog': syslog})
+
+
+if __name__ == '__main__':
+    main()
